f2bd71cb7a4dd67a5763f86c03accc959e1f21a0
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-12-29 01:52:59 +0900

    initial commit

---
+++ src/main/java/me/wolf0023/hardcoreSurvival/HardcoreSurvival.java
@@ -0,0 +1,17 @@
+package me.wolf0023.hardcoreSurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcoreSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
